29663e6ad59104f65423669be65d4facf1f21caf
lishengzxc <eric@lishengcn.cn>
2016-03-25 23:16:52 +0800

    add register

--- controllers/user.go
+++ controllers/user.go
@@ -1,22 +1,37 @@
 package controllers
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"github.com/astaxie/beego"
-	// "gopkg.in/mgo.v2/bson"
-	// "ls-tank-server-go/models"
+	"gopkg.in/mgo.v2/bson"
+	"ls-tank-server-go/models"
 )
 
 type UserController struct {
 	beego.Controller
 }
 
+func encrypt(input string) (ouput string) {
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(input))
+	ouput = hex.EncodeToString(md5Ctx.Sum(nil))
+	return
+}
+
 func (this *UserController) Add() {
-	fmt.Println("add...")
+	ok, data := func() (bool, interface{}) {
+		user := &models.User{}
+		user.Id = bson.NewObjectId()
+		user.Name = this.GetString("name")
+		user.Password = encrypt(this.GetString("password"))
+		return user.Insert()
+	}()
 
 	this.Data["json"] = map[string]interface{}{
-		"ok":   "ok",
-		"data": "add",
+		"ok":   ok,
+		"data": data,
 	}
 	this.ServeJSON()
 }
@@ -30,3 +45,8 @@ func (this *UserController) Get() {
 	}
 	this.ServeJSON()
 }
+
+// todo
+// func isExist(name string) {
+//
+// }
